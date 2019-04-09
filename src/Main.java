847977930ee5a57fc9ef43d1400494bcf311d48c
Monica.Brito <practica2.sistemas@galletasdonde.com>
2019-04-09 11:43:58 -0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import com.sun.demo.jvmti.hprof.Tracker;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Rect;
+
+import javax.swing.*;
+import java.util.Vector;
+
+//https://www.opencv-srf.com/2010/09/object-detection-using-color-seperation.html
+//https://www.tutorialspoint.com/opencv/opencv_using_camera.htm
+public class Main extends Application{
+
+    Mat matrix = null;
+
+    @Override
+    public void start(Stage stage) throws Exception{
+
+    }
+}
